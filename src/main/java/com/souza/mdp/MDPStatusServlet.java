be9753236891e1f818f603c1e4ac8e675472ec81
Edgar Dias de Souza <Edgar Dias de Souza>
2016-03-28 23:23:21 -0300

    first commit

---
+++ src/main/java/com/souza/mdp/MDPStatusServlet.java
@@ -0,0 +1,18 @@
+package com.souza.mdp;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class MDPStatusServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello,the MDP API is running. Version 1-0");
+	}
+
+}
