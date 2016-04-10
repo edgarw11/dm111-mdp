3338311ed46446b0fa3baefecc851d07f216a899
Edgar Dias de Souza <Edgar Dias de Souza>
2016-04-10 19:31:26 -0300

    Finish all operations

--- src/main/java/com/souza/mdp/services/ProductOfInterestManager.java
+++ src/main/java/com/souza/mdp/services/ProductOfInterestManager.java
@@ -1,11 +1,16 @@
 package com.souza.mdp.services;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
@@ -16,6 +21,7 @@ import javax.ws.rs.core.SecurityContext;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
@@ -23,9 +29,11 @@ import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.souza.mdp.models.PriceUpdate;
 import com.souza.mdp.models.ProductOfInterest;
-import com.souza.mdp.models.User;
 
+@Path("productsOfInterest")
 public class ProductOfInterestManager {
 
 	@Context
@@ -33,9 +41,41 @@ public class ProductOfInterestManager {
 
 	public static final String PRODUCTS_OF_INTEREST_KIND = "ProductsOfInterest";
 	public static final String PROP_CPF = "cpf";
-	public static final String PROP_CLIENT_SALES_ID = "clientSalesId";
-	public static final String PROP_PRODUCT_SALES_ID = "productSalesId";
+	public static final String PROP_CLIENT_ID = "clientId";
+	public static final String PROP_PRODUCT_ID = "productId";
 	public static final String PROP_TARGET_PRICE = "targetPrice";
+
+	@POST
+	@Path("priceAlert")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@RolesAllowed({ "ADMIN" })
+	public Status alertPriceUpdated(@Valid PriceUpdate priceUpdate) {
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Filter prodIdFilter = new FilterPredicate(PROP_PRODUCT_ID,
+				FilterOperator.EQUAL, priceUpdate.getProductId());
+		Filter priceFilter = new FilterPredicate(PROP_TARGET_PRICE, FilterOperator.GREATER_THAN_OR_EQUAL,
+				priceUpdate.getNewPrice());
+
+		Filter prodIdAndPriceFilter = CompositeFilterOperator.and(prodIdFilter, priceFilter);
+
+		Query query = new Query(PRODUCTS_OF_INTEREST_KIND)
+				.setFilter(prodIdAndPriceFilter);
+		
+		List<Entity> productsOfInterestEntity = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+		
+		MessageManager messageManager = new MessageManager();
+		if (productsOfInterestEntity != null && productsOfInterestEntity.size() > 0){
+			for (Entity entity : productsOfInterestEntity) {
+				messageManager.sendPriceAlertMessage((String) entity.getProperty(PROP_CPF),
+						priceUpdate);
+			}
+		}
+
+		return Status.OK;
+	}
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
@@ -43,29 +83,31 @@ public class ProductOfInterestManager {
 	@RolesAllowed({ "ADMIN", "USER" })
 	public Status saveOrUpdateProduct(@Valid ProductOfInterest prodOfInterest) {
 
-		UserManager userManager = new UserManager();
-		String userMail = userManager.getUserMailByCpf(prodOfInterest.getCpf());
-
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
+		
+		String userMail = UserManager.getUserMailByCpf(prodOfInterest.getCpf(), datastore);
 
-		datastore = DatastoreServiceFactory.getDatastoreService();
-		Filter cpfFilter = new FilterPredicate(PROP_CPF, FilterOperator.EQUAL,
-				prodOfInterest.getCpf());
-		Filter prodIdFilter = new FilterPredicate(PROP_PRODUCT_SALES_ID,
-				FilterOperator.EQUAL, prodOfInterest.getProductSalesId());
-
-		Filter cpfAndProdIdFilter = CompositeFilterOperator.and(cpfFilter,
-				prodIdFilter);
-
-		Query query = new Query(PRODUCTS_OF_INTEREST_KIND)
-				.setFilter(cpfAndProdIdFilter);
-
-		Entity prodOfInteretEntity = datastore.prepare(query).asSingleEntity();
-
+		if (securityContext == null)
+			System.out.println(" ######## security context ###################");
+		
+		if(securityContext.getUserPrincipal() == null)
+			System.out.println(" ######## getUserPrincipal ###################");
+		
+		if (securityContext.getUserPrincipal().getName() == null)
+			System.out.println(" ######## security context ###################");
+		
+		if (securityContext.isUserInRole("ADMIN"))
+			System.out.println(" ######## ADMIN ###################");
+		
 		// ONLY OWNER OR ADMIN CAN SAVE/UPDATE THE PRODUCT OF INTEREST
 		if (securityContext.getUserPrincipal().getName().equals(userMail)
 				|| securityContext.isUserInRole("ADMIN")) {
+
+			Entity prodOfInteretEntity = getProductOfInterest(
+					prodOfInterest.getCpf(), prodOfInterest.getProductId(),
+					datastore);
+
 			if (prodOfInteretEntity != null) { // EDIT
 
 				productOfInterestToEntity(prodOfInterest, prodOfInteretEntity);
@@ -75,11 +117,11 @@ public class ProductOfInterestManager {
 
 			} else { // CREATE NEW USER
 
-				Key prodOfInterestKey = KeyFactory.createKey(PRODUCTS_OF_INTEREST_KIND,
-						"productOfInterestKey");
+				Key prodOfInterestKey = KeyFactory.createKey(
+						PRODUCTS_OF_INTEREST_KIND, "productOfInterestKey");
 				prodOfInteretEntity = new Entity(PRODUCTS_OF_INTEREST_KIND,
 						prodOfInterestKey);
-				
+
 				productOfInterestToEntity(prodOfInterest, prodOfInteretEntity);
 				datastore.put(prodOfInteretEntity);
 			}
@@ -90,14 +132,113 @@ public class ProductOfInterestManager {
 		return Status.OK;
 	}
 
+	private Entity getProductOfInterest(String cpf, long productId,
+			DatastoreService datastore) {
+		Filter cpfFilter = new FilterPredicate(PROP_CPF, FilterOperator.EQUAL,
+				cpf);
+		Filter prodIdFilter = new FilterPredicate(PROP_PRODUCT_ID,
+				FilterOperator.EQUAL, productId);
+
+		Filter cpfAndProdIdFilter = CompositeFilterOperator.and(cpfFilter,
+				prodIdFilter);
+
+		Query query = new Query(PRODUCTS_OF_INTEREST_KIND)
+				.setFilter(cpfAndProdIdFilter);
+
+		Entity prodOfInteretEntity = datastore.prepare(query).asSingleEntity();
+		return prodOfInteretEntity;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed({ "ADMIN", "USER" })
+	@Path("/bycpf/{cpf}")
+	public List<ProductOfInterest> getProductsByCpf(
+			@PathParam(PROP_CPF) String cpf) {
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		
+		String userMail = UserManager.getUserMailByCpf(cpf, datastore);
+
+		if (securityContext.getUserPrincipal().getName().equals(userMail)
+				|| securityContext.isUserInRole("ADMIN")) {
+
+			List<ProductOfInterest> productsOfInterest = new ArrayList<>();
+			List<Entity> productsOfInterestEntities = listProducts(cpf);
+
+			if (productsOfInterestEntities != null
+					&& productsOfInterestEntities.size() > 0) {
+
+				for (Entity prodOfInterestEntity : productsOfInterestEntities) {
+					ProductOfInterest prodOfInterest = entityToProductOfInterest(prodOfInterestEntity);
+
+					productsOfInterest.add(prodOfInterest);
+				}
+
+				return productsOfInterest;
+
+			} else {
+				throw new WebApplicationException(Status.NOT_FOUND);
+			}
+
+		} else {
+			throw new WebApplicationException(Status.FORBIDDEN);
+		}
+
+	}
+
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{cpf}/{productId}")
+	@RolesAllowed({ "ADMIN", "USER" })
+	public Status deleteUser(@PathParam(PROP_CPF) String cpf,
+			@PathParam(PROP_PRODUCT_ID) Long productId) {
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		
+		String userMail = UserManager.getUserMailByCpf(cpf, datastore);
+
+		if (securityContext.getUserPrincipal().getName().equals(userMail)
+				|| securityContext.isUserInRole("ADMIN")) {
+			
+			Entity prodOfInteretEntity = getProductOfInterest(cpf, productId,
+					datastore);
+
+			if (prodOfInteretEntity != null) {
+
+				datastore.delete(prodOfInteretEntity.getKey());
+				return Status.OK;
+
+			} else {
+				throw new WebApplicationException(Status.NOT_FOUND);
+
+			}
+		} else {
+			throw new WebApplicationException(Status.FORBIDDEN);
+		}
+	}
+
+	private List<Entity> listProducts(String cpf) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Filter cpfFilter = new FilterPredicate(PROP_CPF, FilterOperator.EQUAL,
+				cpf);
+		Query query = new Query(PRODUCTS_OF_INTEREST_KIND).setFilter(cpfFilter)
+				.addSort(PROP_PRODUCT_ID, SortDirection.ASCENDING);
+		List<Entity> userEntity = datastore.prepare(query).asList(
+				FetchOptions.Builder.withDefaults());
+
+		return userEntity;
+	}
+
 	private void productOfInterestToEntity(ProductOfInterest prodOfInterest,
 			Entity userEntity) {
 
 		userEntity.setProperty(PROP_CPF, prodOfInterest.getCpf());
-		userEntity.setProperty(PROP_PRODUCT_SALES_ID,
-				prodOfInterest.getProductSalesId());
-		userEntity.setProperty(PROP_CLIENT_SALES_ID,
-				prodOfInterest.getClientSalesId());
+		userEntity.setProperty(PROP_PRODUCT_ID, prodOfInterest.getProductId());
+		userEntity.setProperty(PROP_CLIENT_ID, prodOfInterest.getClientId());
 		userEntity.setProperty(PROP_TARGET_PRICE,
 				prodOfInterest.getTargetPrice());
 
@@ -107,11 +248,11 @@ public class ProductOfInterestManager {
 		ProductOfInterest prodOfInterest = new ProductOfInterest();
 
 		prodOfInterest.setCpf((String) userEntity.getProperty(PROP_CPF));
-		prodOfInterest.setProductSalesId((Long) userEntity
-				.getProperty(PROP_PRODUCT_SALES_ID));
-		prodOfInterest.setClientSalesId((Long) userEntity
-				.getProperty(PROP_CLIENT_SALES_ID));
-		prodOfInterest.setTargetPrice((Float) userEntity
+		prodOfInterest.setProductId((Long) userEntity
+				.getProperty(PROP_PRODUCT_ID));
+		prodOfInterest.setClientId((Long) userEntity
+				.getProperty(PROP_CLIENT_ID));
+		prodOfInterest.setTargetPrice((Double) userEntity
 				.getProperty(PROP_TARGET_PRICE));
 
 		return prodOfInterest;
